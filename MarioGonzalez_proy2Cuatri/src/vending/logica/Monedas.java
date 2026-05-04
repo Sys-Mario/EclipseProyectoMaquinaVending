851b73c180837c6c5e450c8d436ad5994619ac81
Mario <mariog.iglesia@gmail.com>
2026-05-04 13:08:32 +0200

    Modificando, no proceso **Pensando**

--- MarioGonzalez_proy2Cuatri/src/vending/logica/Monedas.java
+++ MarioGonzalez_proy2Cuatri/src/vending/logica/Monedas.java
@@ -1,23 +1,6 @@
 package vending.logica;
 
-import java.math.BigDecimal;
-
 public enum Monedas {
 	
-	DOS_EUROS("2.00"),
-	UN_EURO("1.00"),
-	CINCUENTA_CENT("0.50"),
-	VEINTE_CENT("0.20"),
-	DIEZ_CENT("0.10"),
-	CINCO_CENT("0.05");
-	
-	private final BigDecimal valor;
-	
-	Monedas (String valorTexto) {
-	    this.valor = new BigDecimal(valorTexto);
-	}
-	
-	public BigDecimal getValor() {
-	    return valor;
-	}
+	A, B, C, D, E, F, G, H
 }
